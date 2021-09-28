0ea8de27aba12ac511e0916c6d6e7089019a8360
MarcoElias <88390686+MEliasM@users.noreply.github.com>
2021-09-28 13:36:04 -0600

    Add files via upload

---
+++ Prototipo2P/MVC/CapaModelo/clsConexion.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Odbc;
+
+namespace CapaModelo
+{
+    class clsConexion
+    {
+        public OdbcConnection conexion()
+        {
+            OdbcConnection conn = new OdbcConnection("Dsn=Segundo_Parcial");
+            try
+            {
+                conn.Open();
+            }
+            catch (OdbcException)
+            {
+                Console.WriteLine("No Conecto");
+            }
+            return conn;
+        }
+
+        public void desconexion(OdbcConnection conn)
+        {
+            try
+            {
+                conn.Close();
+            }
+            catch (OdbcException)
+            {
+                Console.WriteLine("No Conecto");
+            }
+        }
+    }
+}
